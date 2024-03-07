7c9f90bcc377d9695bc0e3cc10785396a7d1cd12
Oleksandr-Santalov <145372683+Oleksandr-Santalov@users.noreply.github.com>
2024-03-07 12:34:41 +0000

    Deploying to gh-pages from @ Oleksandr-Santalov/goit-js-hw-10@f0b52c62251acf4cd41b15600cd174df63cd1f26 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{i}from"./assets/vendor-77e16229.js";const t=document.querySelector(".form");t.addEventListener("submit",n);function n(o){o.preventDefault();let s=t.elements.delay.value,r=t.elements.state.value;new Promise((e,m)=>{setTimeout(()=>{r==="fulfilled"?e(`✅ Fulfilled promise in ${s}ms`):m(`❌ Rejected promise in ${s}ms`)},s)}).then(e=>{i.success({position:"topRight",message:e})}).catch(e=>{i.error({position:"topRight",message:e})}),t.reset()}
+//# sourceMappingURL=commonHelpers2.js.map
